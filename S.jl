72f71850f0dd78073e4b8fc8d431f37dd185ca8b
Carlo Baldassi <carlobaldassi@gmail.com>
2022-03-22 01:17:10 +0100

    Simplex rolling on a sphere
    very preliminary version

--- S.jl
+++ S.jl
@@ -43,19 +43,28 @@ function energy(w::Vector, K::Int, trainset::Matrix, trainlabels::Vector)
     return errors / p
 end
 
+function tosphere!(v)
+    n = length(v)
+    v .*= √(n / (v ⋅ v))
+    return v
+end
+
+tosphere(v) = tosphere!(copy(v))
+
 function new_point(ps::Matrix, d::Float64)
     n, y = size(ps)
-    @assert y ≤ n
+    @assert y < n
 
-    ## barycenter
+    ## non-normalized barycenter
     c = mean(ps, dims=2)
+    ## normalized barycenter
+    r = tosphere(c)
 
     ## create a new orthonormal basis
-    ## that spans the y input points
-    ## (only y-1 points are needed)
-    b = zeros(n, y-1)
+    ## for the tangent plane at r
+    b = zeros(n, y)
     for i = 1:(y-1)
-        di = reduce(vcat, ps[:,i] - c)
+        di = ps[:,i] - c
         if !all(di .== 0)
             normalize!(di)
         end
@@ -66,25 +75,67 @@ function new_point(ps::Matrix, d::Float64)
         normalize!(di)
         b[:,i] = di
     end
+    # # check...
+    # if y > 1
+    #     di = ps[:,end] - c
+    #     if !all(di .== 0)
+    #         normalize!(di)
+    #     end
+    #     for j = 1:(y-1)
+    #         dj = b[:,j]
+    #         di .-= (di ⋅ dj) * dj
+    #     end
+    #     @assert maximum(abs.(di)) < 1e-10
+    # end
+    b[:,end] .= normalize(c)
+    # @assert maximum(abs.(b' * b - I)) < 1e-5
 
     ## generate a random direction
     x = randn(n)
 
     ## subtract the components from the
     ## orthonormal basis
-    for i = 1:(y-1)
+    for i = 1:y
         dir = b[:,i]
         x .-= (x ⋅ dir) * dir
+    end
+    normalize!(x)
+
+    function f(λ)
+        new_p = tosphere!(r + λ * x)
+        return sum((acos((new_p ⋅ ps[:,i]) / n) - d)^2 for i = 1:y)
+    end
+
+    λ = d * √n
+    f₀ = f(λ)
+    # @show f₀
+    δ = 1e-6
+    η = 1.0
+    while f₀ > 1e-12
+        f₁ = f(λ + δ)
+        f′ = (f₁ - f₀) / δ
+        λₙ = λ - η * f₀ / f′
+        fₙ = f(λₙ)
+        # @show fₙ
+        if fₙ < f₀
+            f₀ = fₙ
+            λ = λₙ
+            η *= 1.1
+        else
+            η *= 0.9
+            δ *= 0.95
+            @assert η > 1e-8
+        end
     end
 
     ## rescale x so that its length corresponds
     ## to the height of a regular simplex with
     ## y+1 points and size d
-    normalize!(x)
-    x .*= d * √((y + 1) / 2y)
+    # normalize!(x)
+    # x .*= d * √((y + 1) / 2y)
 
     ## new point
-    new_p = c + x
+    new_p = tosphere!(r + λ * x)
 
     return new_p
 end
@@ -101,20 +152,19 @@ function replace_point!(
         K::Int = 3,
     )
     n, y = size(ps)
-    @assert y+1 ≤ n
+    @assert y ≤ n
     @assert y ≥ 2
     @assert 1 ≤ i_worst ≤ y
 
     ## barycenter, excluding the point to be removed
     vcav = vsum - ps[:,i_worst]
     c = vcav / (y - 1)
+    ## normalized barycenter
+    r = tosphere(c)
 
     ## create a new orthonormal basis
-    ## that spans the y-1 remaining input points
-    ## (only y-2 points are needed; we loop over
-    ## all y points but we skip the i_worst one and
-    ## we also just get out after y-2 points)
-    b = zeros(n, y-2)
+    ## for the tangent plane at r
+    b = zeros(n, y-1)
     k = 0
     for i = 1:y
         i == i_worst && continue
@@ -134,6 +184,7 @@ function replace_point!(
     ## debug checks
     # @assert all(norm(b[:,i]) ≈ 1 for i = 1:(y-2)) # normalization
     # @assert maximum(abs.([b[:,i] ⋅ b[:,j] for i = 1:(y-2), j = 1:(y-2)] - I)) < 1e-12 # orthogonality
+    b[:,end] .= normalize(c)
 
     ## generate a random direction
     x = randn(n)
@@ -144,6 +195,7 @@ function replace_point!(
         dir = b[:,i]
         x .-= (x ⋅ dir) * dir
     end
+    normalize!(x)
     # @assert maximum(abs.(x ⋅ b[:,i] for i = 1:(y-2))) < 1e-12
 
     ## old point
@@ -154,14 +206,41 @@ function replace_point!(
         x = -x
     end
 
+    function f(λ)
+        new_p = tosphere!(r + λ * x)
+        return sum((acos((new_p ⋅ ps[:,i]) / n) - d)^2 for i = 1:y if i ≠ i_worst)
+    end
+
+    λ = d * √n
+    f₀ = f(λ)
+    # @show f₀
+    δ = 1e-6
+    η = 1.0
+    while f₀ > 1e-12
+        f₁ = f(λ + δ)
+        f′ = (f₁ - f₀) / δ
+        λₙ = λ - η * f₀ / f′
+        fₙ = f(λₙ)
+        # @show fₙ
+        if fₙ < f₀
+            f₀ = fₙ
+            λ = λₙ
+            η *= 1.1
+        else
+            η *= 0.9
+            δ *= 0.95
+            @assert η > 1e-8
+        end
+    end
+
     ## rescale x so that its length corresponds
     ## to the height of a regular simplex with
     ## y points and size d
-    normalize!(x)
-    x .*= d * √(y / (2*(y-1)))
+    # normalize!(x)
+    # x .*= d * √(y / (2*(y-1)))
 
     ## new point
-    new_p = c + x
+    new_p = tosphere!(r + λ * x)
 
     ## update the input structures
     ps[:,i_worst] = new_p
@@ -178,7 +257,7 @@ function simplex_opt(
         p::Int,
         y::Int;
         K::Int = 3,
-        d::Float64 = Float64(n),
+        d::Float64 = 0.5,
         seed::Int = 411068089483816338,
         max_attempts::Int = 100,
         rescale_factor::Float64 = 0.5
@@ -193,7 +272,7 @@ function simplex_opt(
 
     ## Create the initial y points
     ## Generate the first point at random
-    ps = reshape(randn(n), (n,1))
+    ps = tosphere!(reshape(randn(n), (n,1)))
 
     ## Add the remaining y-1 points
     for i = 2:y
@@ -201,7 +280,9 @@ function simplex_opt(
     end
 
     ## Check that the distances are all d off-diagonal here
-    @assert all([norm(ps[:,i] - ps[:,j]) ≈ (i==j ? 0.0 : d) for i = 1:y, j = 1:y])
+    @assert all([abs((ps[:,i] ⋅ ps[:,j]) / n) < 1.0 + 1e-8 for i = 1:y, j = 1:y])
+    # @show [acos(clamp(ps[:,i] ⋅ ps[:,j] / n, -1.0, 1.0)) for i = 1:y, j = 1:y]
+    @assert maximum(abs.([acos(clamp(ps[:,i] ⋅ ps[:,j] / n, -1.0, 1.0)) - (i==j ? 0.0 : d) for i = 1:y, j = 1:y])) < 1e-5
 
     ## Pre-compute the sum of all points
     vsum = vec(sum(ps, dims=2))
@@ -224,7 +305,9 @@ function simplex_opt(
         for attempt = 1:max_attempts
             replace_point!(ps, vsum, Es, i_worst, d, trainset, trainlabels, K)
             ## temporary consistency check
-            # @assert all([norm(ps[:,i] - ps[:,j]) ≈ (i==j ? 0.0 : d) for i = 1:y, j = 1:y])
+            @assert all([abs((ps[:,i] ⋅ ps[:,j]) / n) < 1.0 + 1e-8 for i = 1:y, j = 1:y])
+            # @show [acos(clamp(ps[:,i] ⋅ ps[:,j] / n, -1.0, 1.0)) for i = 1:y, j = 1:y]
+            @assert maximum(abs.([acos(clamp(ps[:,i] ⋅ ps[:,j] / n, -1.0, 1.0)) - (i==j ? 0.0 : d) for i = 1:y, j = 1:y])) < 1e-5
             E_new = Es[i_worst]
             E_new < E_worst && break
         end
@@ -253,15 +336,18 @@ function simplex_opt(
 
         @info "resampling failed, rescale simplex $d -> $(d * rescale_factor)"
 
-        d *= rescale_factor
-        ref = Ec ≤ E_best ? c : ps[:,i_best]
+        # d *= rescale_factor
+        # ref = Ec ≤ E_best ? c : ps[:,i_best]
+        ref = c
         for j = 1:y
-            ps[:,j] .= ref .+ rescale_factor .* (ps[:,j] .- ref)
+            ps[:,j] .= tosphere!(ref .+ rescale_factor .* (ps[:,j] .- ref))
         end
+        d = mean([acos(clamp(ps[:,i] ⋅ ps[:,j] / n, -1.0, 1.0)) for i = 1:y for j = 1:y if i ≠ j])
         vsum = vec(sum(ps, dims=2))
         Es = [energy(ps[:,i], K, trainset, trainlabels) for i = 1:y]
         Ec = energy(vsum / y, K, trainset, trainlabels)
-        @assert all([norm(ps[:,i] - ps[:,j]) ≈ (i==j ? 0.0 : d) for i = 1:y, j = 1:y])
+        # @assert all([norm(ps[:,i] - ps[:,j]) ≈ (i==j ? 0.0 : d) for i = 1:y, j = 1:y])
+        @assert maximum(abs.([acos(clamp(ps[:,i] ⋅ ps[:,j] / n, -1.0, 1.0)) - (i==j ? 0.0 : d) for i = 1:y, j = 1:y])) < 1e-5
 
         # open(filename, "a") do io
         #     println(io, "it = $it d = $d Ec = $Ec Es = $Es")
